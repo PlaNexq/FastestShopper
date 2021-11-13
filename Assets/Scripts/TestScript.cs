aacc9879fcd1245fca2322c29a884ef0f7961946
Wesdmond <SHto2004@ya.ru>
2021-11-13 05:30:27 +0500

    add some things
    add wallpaper and logo idle animation.

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    int m_x = 5;
+    //public static GameObject instance = this;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
